9227235d43ee88a34cdc5866e01fd4ade26ae242
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-07-13 00:54:35 +0530

    Add HeroHeader component

---
+++ src/app/HeroHeader/hero-header.component.tsx
@@ -0,0 +1,9 @@
+import styles from './hero-header.module.scss'
+
+export default function HeroHeader() {
+    return (
+        <div className={`${styles.mainContainer}`}>
+
+        </div>
+    )
+}
