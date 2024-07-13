7498f09506b2145d06ad37d840b2e52664dbd08b
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-07-13 19:22:52 +0530

    Update HeroHeader css

--- src/app/HeroHeader/hero-header.component.tsx
+++ src/app/HeroHeader/hero-header.component.tsx
@@ -2,8 +2,10 @@ import styles from './hero-header.module.scss'
 
 export default function HeroHeader() {
     return (
-        <div className={`${styles.mainContainer} h-8`}>
-            <div className={`${styles.heroText} pl-24 text-2xl my-auto`}>I'M <span className={`${styles.heroName} text-4xl`}>Shaikh Yakub Hossain</span><br />Full Stack Developer</div>
+        <div className={`${styles.mainContainer} flex justify-between w-full h-96 px-32`}>
+            <div className={`${styles.heroTextAndBtnContainer} my-auto`}>
+                <div className={`${styles.heroText} text-2xl`}>I'M <span className={`${styles.heroName} text-4xl`}>Shaikh Yakub Hossain</span><br />Full Stack Developer</div>
+            </div>
             <div className={`${styles.heroImage}`}><img className={`h-96`} src="https://cdn.pixabay.com/photo/2016/11/14/17/39/person-1824147_960_720.png" alt="test-image" /></div>
         </div>
     )
