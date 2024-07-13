230e44ecddddee4eb2cc6b52ebc4be96c14ff666
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-07-13 19:40:16 +0530

    Update HeroHeader and Button component css

--- src/app/Button/button.component.tsx
+++ src/app/Button/button.component.tsx
@@ -6,8 +6,8 @@ type propsType = {
 
 export default function Button(props: propsType): JSX.Element {
     return (
-        <div className={`${styles.mainContainer} `}>
-            <button className={`bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded`}>{props.children}</button>
+        <div className={`${styles.mainContainer}`}>
+            <button className={`bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border-4 border-blue-500 hover:border-transparent rounded`}>{props.children}</button>
         </div>
     );
 }
--- src/app/HeroHeader/hero-header.component.tsx
+++ src/app/HeroHeader/hero-header.component.tsx
@@ -1,10 +1,12 @@
-import styles from './hero-header.module.scss'
+import styles from './hero-header.module.scss';
+import Button from '../Button/button.component';
 
 export default function HeroHeader() {
     return (
         <div className={`${styles.mainContainer} flex justify-between w-full h-96 px-32`}>
             <div className={`${styles.heroTextAndBtnContainer} my-auto`}>
-                <div className={`${styles.heroText} text-2xl`}>I'M <span className={`${styles.heroName} text-4xl`}>Shaikh Yakub Hossain</span><br />Full Stack Developer</div>
+                <div className={`${styles.heroText} my-6 text-2xl`}>I'M <span className={`${styles.heroName} text-4xl`}>Shaikh Yakub Hossain</span><br />Full Stack Developer</div>
+                <Button>Contact Me</Button>
             </div>
             <div className={`${styles.heroImage}`}><img className={`h-96`} src="https://cdn.pixabay.com/photo/2016/11/14/17/39/person-1824147_960_720.png" alt="test-image" /></div>
         </div>
