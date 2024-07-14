f09431361df773a73ea66490468389b2ce08d8d2
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-07-14 07:54:38 +0530

    Add About component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import './globals.css'
 import HeroHeader from "./component/HeroHeader/hero-header.component";
+import About from './component/About/about.component';
 
 export default function Home() {
     return (
-        <div className={`bg-slate-800`}>
+        <div className={`bg-slate-800 text-white`}>
             <HeroHeader />
+            <About />
         </div>
     );
 };
