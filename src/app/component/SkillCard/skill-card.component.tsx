2d1b03d9b15fec0d8b0f831c8d1881299082c2ea
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-07-20 11:35:10 +0530

    Change svg icon to base64 icon

--- src/app/component/SkillCard/skill-card.component.tsx
+++ src/app/component/SkillCard/skill-card.component.tsx
@@ -2,14 +2,14 @@ import styles from './skill-card.module.scss';
 
 
 type propsType = {
-    skillIcon: React.ReactNode,
+    skillIcon: string,
     skillTitle: string
 }
 
 export default function SkillCard(props: propsType): JSX.Element {
     return (
         <div className={`${styles.mainContainer} py-7 px-12 rounded shadow-lg shadow-slate-600`}>
-            <div className={`${styles.skillIconContainer} mx-auto`}><div className={`${styles.skillIconImg}`} >{props.skillIcon}</div></div>
+            <div className={`${styles.skillIconContainer} mx-auto`}><img className={`${styles.skillIconImg}`} src={props.skillIcon} /></div>
             <div className={`${styles.skillTitle} mt-5`}>{props.skillTitle}</div>
         </div>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import SkillCard from './component/SkillCard/skill-card.component';
 
 export default function Home() {
 
-    const testSvg:React.ReactNode = <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48"><path fill="#212121" d="M18.974,31.5c0,0.828-0.671,1.5-1.5,1.5s-1.5-0.672-1.5-1.5v-14c0-0.653,0.423-1.231,1.045-1.43 c0.625-0.198,1.302,0.03,1.679,0.563l16.777,23.704C40.617,36.709,44,30.735,44,24c0-11-9-20-20-20S4,13,4,24s9,20,20,20 c3.192,0,6.206-0.777,8.89-2.122L18.974,22.216V31.5z M28.974,16.5c0-0.828,0.671-1.5,1.5-1.5s1.5,0.672,1.5,1.5v13.84l-3-4.227 V16.5z"></path></svg>
+    const testBase64:string = 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHg9IjBweCIgeT0iMHB4IiB3aWR0aD0iMTAwIiBoZWlnaHQ9IjEwMCIgdmlld0JveD0iMCAwIDQ4IDQ4Ij4KPHBhdGggZmlsbD0iIzIxMjEyMSIgZD0iTTE4Ljk3NCwzMS41YzAsMC44MjgtMC42NzEsMS41LTEuNSwxLjVzLTEuNS0wLjY3Mi0xLjUtMS41di0xNGMwLTAuNjUzLDAuNDIzLTEuMjMxLDEuMDQ1LTEuNDMgYzAuNjI1LTAuMTk4LDEuMzAyLDAuMDMsMS42NzksMC41NjNsMTYuNzc3LDIzLjcwNEM0MC42MTcsMzYuNzA5LDQ0LDMwLjczNSw0NCwyNGMwLTExLTktMjAtMjAtMjBTNCwxMyw0LDI0czksMjAsMjAsMjAgYzMuMTkyLDAsNi4yMDYtMC43NzcsOC44OS0yLjEyMkwxOC45NzQsMjIuMjE2VjMxLjV6IE0yOC45NzQsMTYuNWMwLTAuODI4LDAuNjcxLTEuNSwxLjUtMS41czEuNSwwLjY3MiwxLjUsMS41djEzLjg0bC0zLTQuMjI3IFYxNi41eiI+PC9wYXRoPgo8L3N2Zz4='
 
     type skillTestArrProps = {
         skillIcon: React.ReactNode,
@@ -28,7 +28,7 @@ export default function Home() {
                 {
                     skillTestArr.map((item, index): JSX.Element => {
                         return (
-                            <SkillCard skillIcon={testSvg} skillTitle={item.skillTitle} />
+                            <SkillCard skillIcon={testBase64} skillTitle={item.skillTitle} />
                         )
                     })
                 }
