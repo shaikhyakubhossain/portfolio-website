210ba2d195f0a6633c6484edca0e8234476a2168
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-07-21 06:30:27 +0530

    Update nav css

--- src/app/component/Nav/nav.component.tsx
+++ src/app/component/Nav/nav.component.tsx
@@ -3,9 +3,9 @@ import styles from './nav.module.scss'
 
 export default function Nav(): JSX.Element {
     return (
-        <div className={`${styles.mainContainer} flex justify-between`}>
-            <div className={`${styles.leftContainer} ${styles.portfolioTextContainer}`}><div>portfolio.</div></div>
-            <div className={`${styles.rightContainer} flex`}>
+        <div className={`${styles.mainContainer} flex justify-between fixed left-0 right-0 px-20 bg-slate-600 h-12`}>
+            <div className={`${styles.leftContainer} ${styles.portfolioTextContainer} my-auto`}><div>portfolio.</div></div>
+            <div className={`${styles.rightContainer} flex my-auto`}>
                 <div>Home</div>
                 <div>About</div>
                 <div>Skills</div>
