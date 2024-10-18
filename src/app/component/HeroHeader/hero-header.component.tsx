d04647150862fc371f3ee79186b1a48762534710
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-10-18 07:33:53 +0530

    Minor changes, update css

--- src/app/component/HeroHeader/hero-header.component.tsx
+++ src/app/component/HeroHeader/hero-header.component.tsx
@@ -1,6 +1,7 @@
 import styles from './hero-header.module.scss';
 import Button from '../Button/button.component';
 import VerticalLinks from '../VerticalLink/vertical-link.component';
+import Link from 'next/link';
 
 type propsType = {
     name: string,
@@ -9,12 +10,14 @@ type propsType = {
 
 export default function HeroHeader(props: propsType): JSX.Element {
     return (
-        <div id="Home" className={`${styles.mainContainer} relative flex justify-between w-full px-32 mt-12`}>
+        <div id="Home" className={`${styles.mainContainer} relative w-full px-32 pt-12 mt-12`}>
+            <div className='flex flex-wrap-reverse justify-between'>
             <div className={`${styles.heroTextAndBtnContainer} my-auto`}>
                 <div className={`${styles.heroText} my-6 text-2xl text-white`}>I&apos;M <span className={`${styles.heroName} text-4xl`}>{props.name}</span><br />{props.subTitle}</div>
-                <Button>Contact Me</Button>
+                <Link href={'#Contact'}><Button customTW='text-yellow-300 hover:text-black hover:bg-yellow-300'>Contact Me</Button></Link>
             </div>
             <div className={`${styles.heroImage} flex `}><img className={`w-96 h-96`} src="https://cdn.pixabay.com/photo/2016/11/14/17/39/person-1824147_960_720.png" alt="test-image" /></div>
+            </div>
             <VerticalLinks />
         </div>
     )
--- src/app/component/ProjectCard/project-card.component.tsx
+++ src/app/component/ProjectCard/project-card.component.tsx
@@ -9,6 +9,7 @@ const ProjectCard = (props: projectList) => {
     return (
         <div className={`${styles.mainContainer} flex flex-col w-96 bg-slate-600 mx-4 my-2`}>
             <div className={`${styles.projectImageContainer}`}><img className={`${styles.projectImage} w-full h-52`} src={props.projectImageUrl} alt="" /></div>
+            <div className={`${styles.detailContainer} p-10 pt-1`}>
             <div className={`${styles.projectTitle} text-2xl`}>{props.projectTitle}</div>
             <div className={`${styles.projectDescription}`}>{props.projectDescription}</div>
             <div className={`${styles.projectTechStackContainer} flex flex-wrap`}>Tech Stack:
@@ -18,8 +19,9 @@ const ProjectCard = (props: projectList) => {
             </div>
             <div className={`${styles.projectLinksContainer} flex flex-wrap justify-evenly`}>
                 {
-                    props.projectLinks.map((item, index) => <div key={index}><Link href={item.url}><Button>{item.name}</Button></Link></div>)
+                    props.projectLinks.map((item, index) => <div key={index}><Link href={item.url}><Button customTW='text-green-200 hover:text-black hover:bg-green-200 border-green-200'>{item.name}</Button></Link></div>)
                 }
+            </div>
             </div>
         </div>
     )
