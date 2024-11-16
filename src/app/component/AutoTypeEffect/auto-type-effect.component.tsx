155dae7f71fd8dc0fa525bd04e2dd7f82edb3939
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-11-16 08:50:59 +0530

    Add useState hooks for shouldErase and indexToAdd, modify useEffect logic to handle typing and erasing text, and separate cursor blinking into a separate useEffect.

--- src/app/component/AutoTypeEffect/auto-type-effect.component.tsx
+++ src/app/component/AutoTypeEffect/auto-type-effect.component.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import styles from './auto-type-effect.module.scss';
 
 type propsType = {
@@ -9,29 +9,53 @@ type propsType = {
 export default function AutoTypeEffect(props: propsType): JSX.Element {
     const elemRef = useRef<HTMLSpanElement | null>(null);
 
+    const [shouldErase, setShouldErase] = useState<boolean>(false);
+    const [indexToAdd, setIndexToAdd] = useState<number>(0);
+
     useEffect(() => {
         const elem = elemRef.current;
         let typingInterval: any;
-        let cursorBlinkingInterval: any;
         if (elem) {
-            let indexToAdd = 0;
             typingInterval = setInterval(() => {
-                if(indexToAdd === props.subTitle.length) {
-                    clearInterval(typingInterval);
-                    return;
+            console.log(indexToAdd, shouldErase);
+            if(indexToAdd === props.subTitle.length - 1) {
+                // clearInterval(typingInterval);
+                setShouldErase(true);
+            }
+            else if(indexToAdd === 1){
+                // clearInterval(typingInterval);
+                setShouldErase(false);
+            }
+
+                if(!shouldErase){
+                    elem.textContent += props.subTitle[indexToAdd];
+                    setIndexToAdd(prev => prev + 1);
                 }
-                elem.textContent += props.subTitle[indexToAdd];
-                indexToAdd++;
+                else{
+                    elem.textContent = elem.textContent && elem.textContent.slice(0, elem.textContent.length - 1);
+                    setIndexToAdd(prev => prev - 1);
+
+                }
             }, 100);
-            cursorBlinkingInterval = setInterval(() => {
-                elem.style.borderRightWidth = elem.style.borderRightWidth === "2px" ? "0px" : "2px";
-            }, 500)
+            
         }
         return () => {
             clearInterval(typingInterval);
+        }
+    }, [shouldErase, indexToAdd]);
+
+    useEffect(() => {
+        const elem = elemRef.current;
+        let cursorBlinkingInterval: any;
+        if(!elem) return;
+        cursorBlinkingInterval = setInterval(() => {
+            elem.style.borderRightWidth = elem.style.borderRightWidth === "2px" ? "0px" : "2px";
+        }, 500);
+        return () => {
             clearInterval(cursorBlinkingInterval);
         }
-    }, [])
+
+    }, []);
 
     return <span ref={elemRef} className={`${styles.typingText} text-yellow-300`}></span>;
 }
