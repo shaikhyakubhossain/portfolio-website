3033bc5fca9adbe0ce94feade87540a524a36df5
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-11-21 16:53:55 +0530

    Update styles for About component and HeroHeader component to include responsive design changes for various screen sizes.

--- src/app/component/About/about.component.tsx
+++ src/app/component/About/about.component.tsx
@@ -12,7 +12,7 @@ export default function About(props: propsType): JSX.Element {
             <div className={`${styles.heading} text-4xl font-bold mb-4 ml-40`}>About</div>
                 <div className={`${styles.paragraphContainer} flex justify-center`}>
                     <div className={`${styles.longDash} flex flex-col justify-center mr-2 h-[34px] `}><div className="w-11 h-[1px] bg-gray-50"></div></div>
-                    <div className={`${styles.paragraph} w-3/4 text-2xl text-justify`}>{props.aboutMe}</div>
+                    <div className={`${styles.paragraph} w-3/4 min-[925px]:text-2xl text-justify`}>{props.aboutMe}</div>
                 </div>
             </div>
         </div>
--- src/app/component/HeroHeader/hero-header.component.tsx
+++ src/app/component/HeroHeader/hero-header.component.tsx
@@ -15,13 +15,13 @@ type propsType = {
 
 export default function HeroHeader(props: propsType): JSX.Element {
     return (
-        <div id="Home" className={`${styles.mainContainer} relative w-full px-32 pt-12 mt-12`}>
-            <div className={`flex flex-wrap-reverse justify-between max-[1040px]:justify-center`}>
+        <div id="Home" className={`${styles.mainContainer} relative w-full min-[925px]:px-32 pt-12 mt-12`}>
+            <div className={`flex justify-between max-[1040px]:flex-col-reverse max-[1040px]:items-center`}>
             <div className={`${styles.heroTextAndBtnContainer} my-auto`}>
-                <div className={`${styles.heroText} my-6 text-2xl text-white`}>I&apos;M <span className={`${styles.heroName} text-4xl`}>{props.name}</span><br />A <AutoTypeEffect subTitles={props.subTitles} /></div>
+                <div className={`${styles.heroText} my-6 text-2xl max-[640px]:text-xl text-white`}>I&apos;M <span className={`${styles.heroName} min-[925px]:text-4xl max-[640px]:text-2xl`}>{props.name}</span><br />A <AutoTypeEffect subTitles={props.subTitles} /></div>
                 <Link href={'#Contact'}><Button customTW='text-yellow-300 hover:!text-black hover:bg-yellow-300'>Contact Me</Button></Link>
             </div>
-            <div className={`${styles.heroImage} flex min-w-[420px] min-h-[420px]`}><Image quality={100}  width={420} height={420} className={`min-w-[420px] min-h-[420px] rounded-full`} src={props.heroImage} alt='hero-image' /></div>
+            <div className={`${styles.heroImage} relative flex min-w-[420px] min-h-[420px]`}><Image quality={100}  width={420} height={420} className={`min-w-[420px] min-h-[420px] max-[460px]:min-w-[30px] max-[460px]:min-h-[300px] rounded-full`} src={props.heroImage} alt='hero-image' /></div>
             </div>
             <VerticalLinks verticalLink={props.verticalLinks} />
         </div>
